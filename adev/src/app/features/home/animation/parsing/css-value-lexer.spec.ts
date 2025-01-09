ad236ccd9b27783e6d95ab67f8d9ee0f7862129d
hawkgs <hawk.dvp@gmail.com>
2025-01-09 13:23:57 +0200

    test(docs-infra): parser
    Test the parser, the lexer and the utils.

---
+++ adev/src/app/features/home/animation/parsing/css-value-lexer.spec.ts
@@ -0,0 +1,65 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {cssValueLexer} from './css-value-lexer';
+
+describe('css-value-lexer', () => {
+  it('should extract the tokens for a simple static value', () => {
+    const tokens = cssValueLexer('block');
+
+    expect(tokens).toEqual(['block']);
+  });
+
+  it('should extract the tokens for a color value', () => {
+    const tokens = cssValueLexer('#ff0000');
+
+    expect(tokens).toEqual(['#ff0000']);
+  });
+
+  it('should extract the tokens for a single numeric integer value', () => {
+    const tokens = cssValueLexer('42px');
+
+    expect(tokens).toEqual([42, 'px']);
+  });
+
+  it('should extract the tokens for a single numberic decimal value', () => {
+    const tokens = cssValueLexer('66.6%');
+
+    expect(tokens).toEqual([66.6, '%']);
+  });
+
+  it('should extract the tokens for a single unitless numberic value', () => {
+    const tokens = cssValueLexer('1337');
+
+    expect(tokens).toEqual([1337]);
+  });
+
+  it('should extract the tokens for a list of numeric values', () => {
+    const tokens = cssValueLexer('42px 13.37rem 0%');
+
+    expect(tokens).toEqual([42, 'px', 13.37, 'rem', 0, '%']);
+  });
+
+  it('should extract the tokens for a simple transform value', () => {
+    const tokens = cssValueLexer('translateX(42%)');
+
+    expect(tokens).toEqual(['translateX', 42, '%']);
+  });
+
+  it('should extract the tokens for a transform value with a single function with multiple parameters', () => {
+    const tokens = cssValueLexer('translate(42%, 0px)');
+
+    expect(tokens).toEqual(['translate', 42, '%', 0, 'px']);
+  });
+
+  it('should extract the tokens for a transform value with multiple functions with multiple parameters', () => {
+    const tokens = cssValueLexer('translate(42%, 0px) scale(1.5) rotate(180deg)');
+
+    expect(tokens).toEqual(['translate', 42, '%', 0, 'px', 'scale', 1.5, 'rotate', 180, 'deg']);
+  });
+});
