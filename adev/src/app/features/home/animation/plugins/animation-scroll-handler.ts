79b05c0b67709a890710bba9ccfed56c6e78144a
hawkgs <hawk.dvp@gmail.com>
2025-01-29 14:05:11 +0200

    fix(docs-infra): scroll plugin spacer height calculations
    Taking into account the height of the rest of the elements seems to be redundant and problematic.

--- adev/src/app/features/home/animation/plugins/animation-scroll-handler.ts
+++ adev/src/app/features/home/animation/plugins/animation-scroll-handler.ts
@@ -1,6 +1,5 @@
 import {Injector, Renderer2, RendererFactory2, ViewContainerRef} from '@angular/core';
 import {WINDOW} from '@angular/docs';
-import {DOCUMENT} from '@angular/common';
 import {Animation} from '../animation';
 import {AnimationPlugin} from './types';
 
@@ -8,11 +7,9 @@ const RESIZE_DEBOUNCE = 500;
 
 export class AnimationScrollHandler implements AnimationPlugin {
   private _win: Window;
-  private _doc: Document;
   private _renderer: Renderer2;
   private _unlisteners: (() => void)[] = [];
   private _scrollHeight: number = 0;
-  private _otherElementsHeight: number = 0;
   private _spacer?: HTMLElement;
   private _resizeDebounceTimeout?: ReturnType<typeof setTimeout>;
 
@@ -27,7 +24,6 @@ export class AnimationScrollHandler implements AnimationPlugin {
     injector: Injector,
   ) {
     this._win = injector.get(WINDOW);
-    this._doc = injector.get(DOCUMENT);
     this._renderer = injector.get(RendererFactory2).createRenderer(null, null);
   }
 
@@ -66,26 +62,16 @@ export class AnimationScrollHandler implements AnimationPlugin {
   /** Creates and stores a spacer that occupies/creates the scrollable space needed for the animation. */
   private _createSpacer() {
     // Create the spacer and append it to the body.
-    const spacer = this._renderer.createElement('div');
-    this._renderer.addClass(spacer, 'anim-scroll-spacer');
-
-    // Since we might have other elements in the viewport,
-    // we set the spacer exactly to the viewport height ...
-    this._renderer.setStyle(spacer, 'height', this._win.innerHeight + 'px');
-    this._hostVcr.element.nativeElement.appendChild(spacer);
-
-    // ... and then we subtract it from the total scroll height ...
-    this._otherElementsHeight = this._doc.body.scrollHeight - this._win.innerHeight;
-    this._spacer = spacer;
-
-    // ... finally, we update the spacer with the
+    this._spacer = this._renderer.createElement('div');
+    this._renderer.addClass(this._spacer, 'anim-scroll-spacer');
     this._updateSpacerHeight();
+
+    this._hostVcr.element.nativeElement.appendChild(this._spacer);
   }
 
   /** Update stored spacer's height. */
   private _updateSpacerHeight() {
-    // Ensures that we have exactly `scrollHeight` scrollable pixels in the viewport.
-    const spacerHeight = this._scrollHeight + this._win.innerHeight - this._otherElementsHeight;
+    const spacerHeight = this._scrollHeight + this._win.innerHeight;
     this._renderer.setStyle(this._spacer, 'height', spacerHeight + 'px');
   }
 }
