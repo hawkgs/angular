324619b1562d2ead8fd5bedfd137e9efe3646e56
hawkgs <hawk.dvp@gmail.com>
2025-01-30 13:29:30 +0200

    feat(docs-infra): implement WAAS layer and meteor field. WIP
    Works at any scale layer and meteor field layer WIP

--- adev/src/app/features/new-anim/definition.ts
+++ adev/src/app/features/new-anim/definition.ts
@@ -1,6 +1,7 @@
 import {AnimationDefinition, Styles} from '../home/animation';
 import {AnimationRule} from '../home/animation/types';
 
+// Layers and layer objects selectors
 const LOGO_LAYER_ID = 'logo';
 const LOGO = `${LOGO_LAYER_ID} >> .logo`;
 const SHIELD = `${LOGO_LAYER_ID} >> .shield`;
@@ -14,6 +15,11 @@ const U_LETTER = `${LOGO_LAYER_ID} >> .u-letter`;
 const L_LETTER = `${LOGO_LAYER_ID} >> .l-letter`;
 const A_LETTER = `${LOGO_LAYER_ID} >> .a-letter`;
 const R_LETTER = `${LOGO_LAYER_ID} >> .r-letter`;
+
+const WORKS_AT_ANY_SCALE_LAYER = 'works-at-any-scale';
+
+const METEOR_FIELD_LAYER = 'meteor-field';
+const METEOR_FIELD = `${METEOR_FIELD_LAYER} >> .field`;
 
 function hideLetter(selector: string, startTime: number): AnimationRule<Styles> {
   const endTime = startTime + 1; // 1 sec duration
@@ -29,64 +35,129 @@ function hideLetter(selector: string, startTime: number): AnimationRule<Styles> 
   };
 }
 
-export const DEFINITION: AnimationDefinition = [
-  {
-    selector: LOGO,
-    timespan: [0, 5],
-    from: {
-      transform: 'translateX(0)',
+/** Generate the animation definition for the home page. */
+export function generateHomeAnimationDefinition(meteorsCount: number): AnimationDefinition {
+  // Logo layer animation
+  const logoLayerAnim: AnimationDefinition = [
+    {
+      selector: LOGO,
+      timespan: [0, 5],
+      from: {
+        transform: 'translateX(0)',
+      },
+      to: {
+        transform: 'translateX(467px)',
+      },
     },
-    to: {
-      transform: 'translateX(467px)',
+    hideLetter(R_LETTER, 1),
+    hideLetter(A_LETTER, 1.5),
+    hideLetter(L_LETTER, 2),
+    hideLetter(U_LETTER, 2.5),
+    hideLetter(G_LETTER, 3),
+    hideLetter(N_LETTER, 3.5),
+    // Make sure that the last letter disappers at the end of layer transition,
+    // i.e. 4 + 1 = 5th second end time
+    hideLetter(CAPITAL_A_LETTER, 4),
+    {
+      selector: SHIELD_MIDDLE,
+      timespan: [5.5, 5.6],
+      from: {
+        transform: 'scale(1)',
+      },
+      to: {
+        transform: 'scale(0)',
+      },
     },
-  },
-  hideLetter(R_LETTER, 1),
-  hideLetter(A_LETTER, 1.5),
-  hideLetter(L_LETTER, 2),
-  hideLetter(U_LETTER, 2.5),
-  hideLetter(G_LETTER, 3),
-  hideLetter(N_LETTER, 3.5),
-  // Make sure that the last letter disappers at the end of layer transition,
-  // i.e. 4 + 1 = 5th second end time
-  hideLetter(CAPITAL_A_LETTER, 4),
-  {
-    selector: SHIELD_MIDDLE,
-    timespan: [5.5, 5.6],
-    from: {
-      transform: 'scale(1)',
+    {
+      selector: SHIELD_BOTTOM_A_ARC,
+      timespan: [5.5, 5.6],
+      from: {
+        transform: 'scaleY(1)',
+      },
+      to: {
+        transform: 'scaleY(0)',
+      },
     },
-    to: {
-      transform: 'scale(0)',
+    {
+      selector: SHIELD_BOTTOM_EXTENSION,
+      timespan: [5.5, 5.6],
+      from: {
+        transform: 'scale(0)',
+      },
+      to: {
+        transform: 'scale(1)',
+      },
     },
-  },
-  {
-    selector: SHIELD_BOTTOM_A_ARC,
-    timespan: [5.5, 5.6],
-    from: {
-      transform: 'scaleY(1)',
+    {
+      selector: SHIELD,
+      timespan: [5.5, 10],
+      from: {
+        transform: 'scale(1) rotate(0deg)',
+      },
+      to: {
+        transform: 'scale(20) rotate(-270deg)',
+      },
     },
-    to: {
-      transform: 'scaleY(0)',
+  ];
+
+  // "Works at any scale" layer animation
+  const waasLayerAnim: AnimationDefinition = [
+    {
+      selector: WORKS_AT_ANY_SCALE_LAYER,
+      timespan: [5, 10],
+      from: {
+        transform: 'scale(0)',
+        opacity: '0',
+      },
+      to: {
+        transform: 'scale(1)',
+        opacity: '1',
+      },
     },
-  },
-  {
-    selector: SHIELD_BOTTOM_EXTENSION,
-    timespan: [5.5, 5.6],
-    from: {
-      transform: 'scale(0)',
+    {
+      selector: WORKS_AT_ANY_SCALE_LAYER,
+      timespan: [14, 15.5],
+      from: {
+        transform: 'scale(1)',
+        opacity: '1',
+      },
+      to: {
+        transform: 'scale(1.5)',
+        opacity: '0',
+      },
     },
-    to: {
-      transform: 'scale(1)',
+  ];
+
+  // Meteor field layer animation
+  const meteorFieldLayerAnim: AnimationDefinition = [
+    {
+      selector: METEOR_FIELD,
+      at: 13,
+      styles: {
+        display: 'flex',
+      },
     },
-  },
-  {
-    selector: SHIELD,
-    timespan: [5.5, 10],
-    from: {
-      transform: 'scale(1) rotate(0deg)',
+    {
+      selector: METEOR_FIELD,
+      timespan: [14, 15],
+      from: {
+        opacity: '0',
+      },
+      to: {
+        opacity: '1',
+      },
     },
-    to: {
-      transform: 'scale(20) rotate(-270deg)',
+    {
+      selector: METEOR_FIELD,
+      timespan: [13, 16],
+      from: {
+        transform: 'scale(1.42)',
+      },
+      to: {
+        transform: 'scale(1)',
+      },
     },
-  },
-];
+  ];
+
+  return [...logoLayerAnim, ...waasLayerAnim, ...meteorFieldLayerAnim];
+}
--- adev/src/app/features/new-anim/new-anim.component.ts
+++ adev/src/app/features/new-anim/new-anim.component.ts
@@ -7,10 +7,17 @@ import {
   viewChildren,
   ViewContainerRef,
 } from '@angular/core';
+import {WINDOW} from '@angular/docs';
+
 import {Animation, AnimationCreatorService, AnimationLayerDirective} from '../home/animation';
 import {AnimationPlayer} from '../home/animation/plugins/animation-player';
 import {AnimationScrollHandler} from '../home/animation/plugins/animation-scroll-handler';
-import {DEFINITION} from './definition';
+import {generateHomeAnimationDefinition} from './definition';
+
+// In pixels. Keep in sync with the SCSS file.
+const METEOR_WIDTH = 120;
+const METEOR_HEIGHT = 170;
+const METEOR_GAP = 85;
 
 @Component({
   selector: 'adev-new-anim',
@@ -21,24 +28,45 @@ import {DEFINITION} from './definition';
   providers: [AnimationCreatorService],
 })
 export class NewAnimComponent implements AfterViewInit {
+  private readonly _win = inject(WINDOW);
   private readonly _animCreator = inject(AnimationCreatorService);
   private readonly _vcr = inject(ViewContainerRef);
   private readonly _injector = inject(Injector);
   private _animation?: Animation;
+  private _meteorsCount = this._calcMeteorsCount();
 
   animationLayers = viewChildren(AnimationLayerDirective);
+  meteors = new Array(this._meteorsCount).fill(null);
 
   ngAfterViewInit() {
     this._animation = this._animCreator
       .createAnimation(this.animationLayers(), {
         timestep: 10,
       })
-      .define(DEFINITION)
+      .define(generateHomeAnimationDefinition(this._win.innerWidth / this._win.innerHeight))
       .addPlugin(new AnimationPlayer(this._vcr))
       .addPlugin(new AnimationScrollHandler(this._vcr, this._injector));
   }
 
   ngOnDestroy() {
     this._animation?.dispose();
+  }
+
+  private _calcMeteorsCount() {
+    const winW = this._win.innerWidth;
+    const winH = this._win.innerHeight;
+    const mW = METEOR_WIDTH + METEOR_GAP;
+    const mH = METEOR_HEIGHT + METEOR_GAP;
+    let rows = 1;
+    let cols = 1;
+
+    while (rows * mW < winW) {
+      rows++;
+    }
+    while (cols * mH < winH) {
+      cols++;
+    }
+
+    return (rows + 1) * cols;
   }
 }
